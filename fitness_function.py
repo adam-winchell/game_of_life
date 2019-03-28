753746a10715b119118f99c51fdafce66f910034
joshladd <joshua.ladd@colorado.edu>
2019-03-28 12:05:29 -0600

    updated ff1

--- fitness_function.py
+++ fitness_function.py
@@ -1,20 +1,46 @@
 import numpy as np
 import pickle
 from collections import defaultdict
+from copy import deepcopy
 from scipy.spatial.distance import pdist
 
 def ff1(data):
 	score = 0
+	result = deepcopy(data[0])
+
 	add1 = np.vectorize(lambda x: x+1 if x > 0 else 0)
 	for d in range(1,len(data)):
-		data[d] = add1(data[d])
+
+
+		for m in result:
+			for n in m:
+				if n > 0:
+					print("%2d" % n,end=' ')
+				else:
+					print('__',end=' ')
+			print()
+		print()
+
+		result = add1(result)
+
+
 		for i in range(data[d].shape[0]):
 			for j in range(data[d].shape[0]):
 				if data[d][i,j] > 0:
-					score += data[d-1][i,j]
-					data[d][i,j] = 1
-	return score
+					result[i,j] = 1
 
+	print(np.max(result))		
+	return np.max(result)/(len(data)-1)
+
+def ff2(data):
+	score = 0
+	add1 = np.vectorize(lambda x: x+1 if x > 0 else 0)
+	for d in range(1,len(data)):
+		data[d] = add1(data[d])
+		lin = list(data[d].ravel())
+		print(np.bincount(lin))
+		#print(counts)
+	return score
 
 def ff3(data):
 	score = 0
@@ -30,19 +56,16 @@ def ff3(data):
 		print(min(dist))
 		print()
 		#print(pdist(dist_arr))
-	return score
+	return 1/score
 
 
-# 	return score
-
-# def ff3(data):
 
 
 
 def main():
 	with open('frames.p','rb') as pFile:
 		data = pickle.load(pFile)
-	print(ff3(data))
+	print(ff1(data))
 
 
 if __name__ == '__main__':
